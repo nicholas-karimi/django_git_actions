0547a245628e389cde6f776a8a8e79a2393a03e8
nicholas-karimi <kariminic@gmail.com>
2022-06-22 14:48:18 +0300

    mod models

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,16 @@
+from django.contrib.auth.models import User
+from django.utils import timezone
 from django.db import models
+
 
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     slug = models.SlugField(max_length=255)
 
     def __str__(self):
         return self.title
+
