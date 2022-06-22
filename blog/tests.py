a0ce3c5304eae5e96f882936d15f87609a2b0e63
nicholas-karimi <kariminic@gmail.com>
2022-06-22 14:15:16 +0300

    ft: add github actions, flake8 and coverage

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Post
 
-# Create your tests here.
+
+class Modeltesting(TestCase):
+    def setUp(self):
+        self.blog = Post.objects.create(title="Xiaomi Flagship Phone", author="Nicholas Karimi", slug='xiaomi-device')
+
+    def test_post_model(self):
+        data = self.blog
+        self.assertTrue(isinstance(data, Post))
+        self.assertEqual(str(data), "Xiaomi Flagship Phone")
